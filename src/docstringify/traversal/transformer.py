fccba31c5d7d2b5c1ea1f10b02eb80dc93ea066f
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2025-05-11 20:27:17 -0400

    Add verbose mode (#29)
    * Add verbose mode
    * Add docstrings for traversal classes

--- src/docstringify/traversal/transformer.py
+++ src/docstringify/traversal/transformer.py
@@ -1,3 +1,8 @@
+"""
+Traverse the AST with the ability to transform it to add templates for docstrings based
+on the source code.
+"""
+
 from __future__ import annotations
 
 import ast
@@ -11,16 +16,39 @@ if TYPE_CHECKING:
 
 
 class DocstringTransformer(ast.NodeTransformer, DocstringVisitor):
+    """
+    A class for indicating where docstrings are missing in a single module of source code
+    and injecting suggested docstring templates based on the AST representation into the
+    source code.
+
+    Parameters
+    ----------
+    filename : str
+        The file to process.
+    converter : type[DocstringConverter]
+        The converter class determines the docstring style to use for generating the
+        suggested docstring templates.
+    overwrite : bool, keyword-only, default=False
+        Whether to save the modified source code back to the original file.
+    verbose : bool, keyword-only, default=False
+        Whether to run in verbose mode.
+    """
+
     def __init__(
         self,
         filename: str,
         converter: type[DocstringConverter],
+        *,
         overwrite: bool = False,
+        verbose: bool = False,
     ) -> None:
-        super().__init__(filename, converter)
-        self.overwrite = overwrite
+        super().__init__(filename, converter, verbose=verbose)
+
+        self.overwrite: bool = overwrite
+        """Whether to save the modified source code back to the original file."""
 
     def save(self) -> None:
+        """Save the modified AST to a file as source code."""
         if self.missing_docstrings:
             output = (
                 self.source_file
@@ -37,6 +65,23 @@ class DocstringTransformer(ast.NodeTransformer, DocstringVisitor):
             print(f'Docstring templates written to {output}')
 
     def handle_missing_docstring(self, docstring_node: DocstringNode) -> DocstringNode:
+        """
+        Handle missing docstrings by injecting a suggested docstring template based on
+        the source code into the AST.
+
+        Parameters
+        ----------
+        docstring_node : DocstringNode
+            An instance of :class:`.DocstringNode`, which wraps an AST node and adds
+            additional context relevant for Docstringify.
+
+        Returns
+        -------
+        DocstringNode
+            An instance of :class:`.DocstringNode`, which wraps an AST node and adds
+            additional context relevant for Docstringify. The AST node it contains will
+            have a new node for the docstring template added to its body.
+        """
         suggested_docstring = self.docstring_converter.suggest_docstring(
             docstring_node,
             indent=0
@@ -57,5 +102,9 @@ class DocstringTransformer(ast.NodeTransformer, DocstringVisitor):
         return docstring_node
 
     def process_file(self) -> None:
+        """
+        Process a source code file, handling missing docstrings and saving the modified
+        AST to a file.
+        """
         super().process_file()
         self.save()
