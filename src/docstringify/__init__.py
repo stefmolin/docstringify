89991267b54304e5523bfcbeed6fe29c246e8b5d
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2025-07-11 01:11:33 -0400

    Switch to hatchling and uv; cleanup numpydoc settings (#52)
    * Switch to hatchling and uv
    * Remove override that isn't necessary in numpydoc>=1.9.0

--- src/docstringify/__init__.py
+++ src/docstringify/__init__.py
@@ -3,4 +3,8 @@ Flag missing docstrings and, optionally, generate them from signatures and
 type annotations.
 """
 
-__version__ = '1.0.0'
+import importlib.metadata
+
+__version__ = importlib.metadata.version(__name__)
+
+__all__ = ['__version__']
