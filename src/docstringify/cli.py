63dafd0e7254135067eb42ce502fdebfaffd4512
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2025-07-12 18:42:32 -0700

    Rework CLI to use subcommands

--- src/docstringify/cli.py
+++ src/docstringify/cli.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 
 import argparse
 import sys
+from enum import StrEnum, auto
 from functools import partial
 from typing import TYPE_CHECKING
 
@@ -34,6 +35,188 @@ STYLES: dict[
 CLI_DEFAULTS = {'threshold': 1.0}
 
 
+class DocstringifyRunModes(StrEnum):
+    """Run modes for Docstringify's CLI."""
+
+    CHECK = auto()
+    EDIT = auto()
+    SUGGEST = auto()
+
+
+def _process_files(
+    mode: DocstringifyRunModes, args: argparse.Namespace
+) -> tuple[int, int]:
+    """
+    Process the filenames passed in as command line arguments.
+
+    Parameters
+    ----------
+    mode : DocstringifyRunModes
+        The mode in which to run Docstringify.
+    args : argparse.Namespace
+        The command line arguments.
+
+    Returns
+    -------
+    tuple[int, int]
+        A tuple containing the number of missing docstrings and the total number of
+        docstrings expected.
+    """
+    get_docstring_processor = (
+        partial(
+            DocstringTransformer,
+            converter=STYLES[args.style],
+            overwrite=bool(args.overwrite),
+            verbose=args.verbose,
+        )
+        if mode == DocstringifyRunModes.EDIT
+        else partial(
+            DocstringVisitor,
+            converter=None
+            if mode == DocstringifyRunModes.CHECK
+            else STYLES[args.style],
+            verbose=args.verbose,
+        )
+    )
+
+    docstrings_expected = missing_docstrings = 0
+    for file in args.filenames:
+        processor = get_docstring_processor(file)
+        processor.process_file()
+        missing_docstrings += len(processor.missing_docstrings)
+        docstrings_expected += processor.docstrings_inspected
+
+    return missing_docstrings, docstrings_expected
+
+
+def _run(mode: DocstringifyRunModes, args: argparse.Namespace) -> int:
+    """
+    Run Docstringify in the specified mode with the provided command line arguments.
+
+    Parameters
+    ----------
+    mode : DocstringifyRunModes
+        The run mode.
+    args : argparse.Namespace
+        The command line arguments.
+
+    Returns
+    -------
+    int
+        Exit code for the process, where non-zero values indicate errors.
+    """
+    missing_docstrings, docstrings_expected = _process_files(mode, args)
+
+    if mode == DocstringifyRunModes.CHECK:
+        if (
+            docstrings_expected
+            and (missing_percentage := (missing_docstrings / docstrings_expected))
+            > 1 - args.threshold
+        ):
+            print(f'Missing {missing_percentage:.0%} of docstrings', file=sys.stderr)
+            print(
+                f'Your settings require {args.threshold:.0%} of docstrings to be present',
+                file=sys.stderr,
+            )
+            return 1
+        return 0
+
+    # modes suggest and edit behave the same
+    if docstrings_expected and missing_docstrings:
+        return 1
+    return 0
+
+
+def _populate_subparser(
+    subparsers: argparse._SubParsersAction[argparse.ArgumentParser],
+    subcommand: DocstringifyRunModes,
+    help_msg: str,
+) -> argparse.ArgumentParser:
+    """
+    Populate an argument subparser for handling subcommands from the main CLI.
+
+    Parameters
+    ----------
+    subparsers : argparse._SubParsersAction[argparse.ArgumentParser]
+        The result of calling the :meth:`argparse.ArgumentParser.add_subparsers` method
+        on the main argument parser.
+    subcommand : DocstringifyRunModes
+        The run mode for which to populate the parser.
+    help_msg : str
+        The help message to include for the subcommand.
+
+    Returns
+    -------
+    argparse.ArgumentParser
+        The populated subparser.
+    """
+    parser = subparsers.add_parser(subcommand, help=help_msg)
+
+    parser.add_argument(
+        'filenames', nargs='+', metavar='filename', help='filename(s) to process'
+    )
+
+    if subcommand == DocstringifyRunModes.CHECK:
+        parser.add_argument(
+            '--threshold',
+            type=float,
+            default=CLI_DEFAULTS['threshold'],
+            help='the percentage of docstrings that must be present to pass',
+        )
+    else:
+        if subcommand == DocstringifyRunModes.EDIT:
+            parser.add_argument(
+                '--overwrite',
+                action='store_true',
+                help='whether to overwrite the existing file with the changes',
+            )
+        parser.add_argument(
+            '--style',
+            choices=STYLES.keys(),
+            required=True,
+            help='docstring style to use',
+        )
+
+    parser.add_argument('--verbose', action='store_true', help='run in verbose mode')
+    parser.set_defaults(run=partial(_run, subcommand))
+
+    return parser
+
+
+def _create_parser() -> argparse.ArgumentParser:
+    """
+    Create an argument parser for the CLI.
+
+    Returns
+    -------
+    argparse.ArgumentParser
+        The argument parser for the CLI.
+    """
+    parser = argparse.ArgumentParser(prog=PROG, description=pkg_description)
+    parser.add_argument(
+        '--version', action='version', version=f'%(prog)s {__version__}'
+    )
+
+    subparsers = parser.add_subparsers(title='modes')
+    for mode, help_msg in [
+        (
+            DocstringifyRunModes.CHECK,
+            'use Docstringify to check whether files have the required docstrings',
+        ),
+        (
+            DocstringifyRunModes.EDIT,
+            'use Docstringify to edit files to inject docstring templates for you',
+        ),
+        (
+            DocstringifyRunModes.SUGGEST,
+            'use Docstringify to review files and suggest templates for missing docstrings',
+        ),
+    ]:
+        _populate_subparser(subparsers, mode, help_msg)
+
+    return parser
+
+
 def main(argv: Sequence[str] | None = None) -> int:
     """
     Flag missing docstrings and, optionally, generate them from signatures and
@@ -47,81 +230,11 @@ def main(argv: Sequence[str] | None = None) -> int:
     Returns
     -------
     int
-        Exit code for the process, where non-zero values indicate errors, and ``1``
-        indicates that more than the allowed percentage of docstrings were missing.
+        Exit code for the process, where non-zero values indicate errors.
     """
-
-    parser = argparse.ArgumentParser(prog=PROG, description=pkg_description)
-    parser.add_argument('filenames', nargs='*', help='Filenames to process')
-    parser.add_argument(
-        '-v', '--verbose', action='store_true', help='Run in verbose mode'
-    )
-    parser.add_argument(
-        '--version', action='version', version=f'%(prog)s {__version__}'
-    )
-
-    run_group = parser.add_argument_group('Run options')
-    handle_missing_docstring = run_group.add_mutually_exclusive_group(required=False)
-    handle_missing_docstring.add_argument(
-        '--make-changes',
-        choices=STYLES.keys(),
-        help='Whether to insert docstring templates for items missing docstrings',
-    )
-    handle_missing_docstring.add_argument(
-        '--make-changes-inplace',
-        choices=STYLES.keys(),
-        help=(
-            'Whether to insert docstring templates for items missing docstrings, '
-            'overwriting the original file'
-        ),
-    )
-    handle_missing_docstring.add_argument(
-        '--suggest-changes',
-        choices=STYLES.keys(),
-        help='Whether to print out docstring templates for items missing docstrings',
-    )
-    run_group.add_argument(
-        '--threshold',
-        type=float,
-        default=CLI_DEFAULTS['threshold'],
-        help='The percentage of docstrings that must be present to pass',
-    )
+    parser = _create_parser()
     args = parser.parse_args(argv)
-
-    get_docstring_processor = (
-        partial(
-            DocstringTransformer,
-            converter=STYLES[style],
-            overwrite=bool(args.make_changes_inplace),
-            verbose=args.verbose,
-        )
-        if (style := args.make_changes or args.make_changes_inplace)
-        else partial(
-            DocstringVisitor,
-            converter=STYLES.get(args.suggest_changes),
-            verbose=args.verbose,
-        )
-    )
-
-    docstrings_processed = missing_docstrings = 0
-    for file in args.filenames:
-        processor = get_docstring_processor(file)
-        processor.process_file()
-        missing_docstrings += len(processor.missing_docstrings)
-        docstrings_processed += processor.docstrings_inspected
-
-    if (
-        docstrings_processed
-        and (missing_percentage := (missing_docstrings / docstrings_processed))
-        > 1 - args.threshold
-    ):
-        print(f'Missing {missing_percentage:.0%} of docstrings', file=sys.stderr)
-        print(
-            f'Your settings require {args.threshold:.0%} of docstrings to be present',
-            file=sys.stderr,
-        )
-        return 1
-    return 0
+    return args.run(args)
 
 
 if __name__ == '__main__':
